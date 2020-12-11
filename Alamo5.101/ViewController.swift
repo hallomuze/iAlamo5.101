125340b1704da2aa2f924e4523ef8baa59bbe009
muzna <hallomuze@gmail.com>
2020-12-11 17:31:26 +0900

    Initial Commit

---
+++ Alamo5.101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamo5.101
+//
+//  Created by muzna on 2020/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
