c8cd03210d7ef9818933ddc309efd4d4ed6577c1
muzna <hallomuze@gmail.com>
2020-12-11 18:05:43 +0900

    add model except secretkey

--- Alamo5.101/ViewController.swift
+++ Alamo5.101/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+ 
 }
 
+extension ViewController {
+    func fetch() {
+        
+        let reqUrl = "https://api.unsplash.com/photos"
+        //let secureId = "asdfasdfasdfasdfasdf"
+        let parameters: Parameters = [
+            "client_id": unsplashKey, // security , 씨케
+            "page": "3",
+            "per_page": "20"
+        ]
+        
+        let resp = AF.request(reqUrl, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil, interceptor: nil, requestModifier: nil)
+        
+        resp.responseJSON { response in
+           // print(json.data)
+            
+            guard let dataOfResponse = response.data else{
+                return
+            }
+            do {
+                let final = try JSONDecoder().decode([UnsplashModel].self, from: dataOfResponse)
+                print("final:\(final)")
+
+            } catch let error {
+                print("Error result: \(error)")
+                //wLog("decoding err:\(error.localizedDescription)")
+            }
+            
+        }
+}
+}
