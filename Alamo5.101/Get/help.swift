db26f9a52d2a8f9589f987bc2a18b2257e88b07a
muzna <hallomuze@gmail.com>
2020-12-12 13:03:47 +0900

    help add

---
+++ Alamo5.101/Get/help.swift
@@ -0,0 +1,18 @@
+//
+//  jsonRaw.swift
+//  Alamo5.101
+//
+//  Created by muzna on 2020/12/11.
+//
+
+ 
+ 
+/*
+ https://stackoverflow.com/questions/60960976/swift-5-alamofire-5-get-method-error-alamofire-aferror-urlrequestvalidation
+ Swift 5 & Alamofire 5 : GET method ERROR: Alamofire.AFError.URLRequestValidationFailureReason.bodyDataInGETRequest(22 bytes)
+ 
+ 
+ Alamofire 5 and Apple's 2019 frameworks now produce an error when you try to make a GET request with body data, as such a request is invalid. I would suggest checking to make sure that's what your server is expected, and if it does really require body data for GET requests, reach out to the API provider and request a change, as no device running Apple's 2019 OSes will be able to make such a request.
+ 
+ */
+     
